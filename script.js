4ffbf82566c3d579074e75bb41fc2bc5e2277f14
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-06-23 14:28:08 -0700

    Remove redundant required attribute

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const emailField_input = document.getElementById("emailField");
 const phoneField_input = document.getElementById("phoneField");
 const password1Field_input = document.getElementById("password1Field");
 const password2Field_input = document.getElementById("password2Field");
-
 const submitButton_button = document.getElementById("submitButton");
 const loginPrompt_link = document.getElementById("loginPrompt");
+
+const mainForm_form = document.getElementById("mainForm")
+
 
