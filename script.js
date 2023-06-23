432bead8ff230c6f3056a0f640ad9c27020c1082
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-06-23 12:17:10 -0700

    Cache the dom

---
+++ script.js
@@ -0,0 +1,9 @@
+const firstNameField_input = document.getElementById("firstNameField");
+const lastNameField_input = document.getElementById("lastNameField");
+const emailField_input = document.getElementById("emailField");
+const phoneField_input = document.getElementById("phoneField");
+const password1Field_input = document.getElementById("password1Field");
+const password2Field_input = document.getElementById("password2Field");
+
+const submitButton_button = document.getElementById("submitButton");
+const loginPrompt_link = document.getElementById("loginPrompt");
